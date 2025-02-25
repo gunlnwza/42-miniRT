97b4594e55afecc9e749163d1c7165285134705d
Natachat Teecharcharn <techazuza@gmail.com>
2025-02-25 15:55:04 +0700

    Bring back real main

--- srcs/main.c
+++ srcs/main.c
@@ -6,48 +6,27 @@
 /*   By: nteechar <techazuza@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 18:49:26 by nteechar          #+#    #+#             */
-/*   Updated: 2025/02/25 15:54:12 by nteechar         ###   ########.fr       */
+/*   Updated: 2025/02/25 15:54:45 by nteechar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/mini_rt.h"
 
-// int	main(int argc, char **argv)
-// {
-// 	t_data	data;
-
-// 	ft_bzero(&data, sizeof(t_data));
-// 	if (argc != 2 || open_file(&data.world, argv[1]) != 0)
-// 	{
-// 		ft_putstr_fd("Error: Invalid scene file\n", STDERR_FILENO);
-// 		return (EXIT_FAILURE);
-// 	}
-// 	if (init_display(&data) == ERROR)
-// 		return (EXIT_FAILURE);
-// 	configure_camera(&data.world.camera);
-// 	render_image(data.img, &data.world);
-// 	mlx_loop(data.mlx);
-// 	mlx_terminate(data.mlx);
-// 	return (EXIT_SUCCESS);
-// }
-
 int	main(int argc, char **argv)
 {
-	int	i = 1;
+	t_data	data;
 
-	while (i < argc)
+	ft_bzero(&data, sizeof(t_data));
+	if (argc != 2 || open_file(&data.world, argv[1]) != 0)
 	{
-		double a = atof(argv[i]); 
-		double b = ft_atof(argv[i]);
-
-		if (a == b)
-			printf("%i. OK (a = %f, b = %f)\n", i, a, b);
-		else
-			printf("%i. \033[31mKO\033[0m (a = %f, b = %f)\n", i, a, b);
-
-		i++;
+		ft_putstr_fd("Error: Invalid scene file\n", STDERR_FILENO);
+		return (EXIT_FAILURE);
 	}
-	// printf("\n");
-
+	if (init_display(&data) == ERROR)
+		return (EXIT_FAILURE);
+	configure_camera(&data.world.camera);
+	render_image(data.img, &data.world);
+	mlx_loop(data.mlx);
+	mlx_terminate(data.mlx);
 	return (EXIT_SUCCESS);
 }
